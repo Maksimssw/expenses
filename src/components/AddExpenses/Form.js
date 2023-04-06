42430e7cb69a5eaa576683fc0283e4717f2d341c
Maks <kiva_hill@mail.ru>
2023-04-06 16:37:28 +0300

    feat(Form): add Handler & get data

--- src/components/AddExpenses/Form.js
+++ src/components/AddExpenses/Form.js
@@ -1,5 +1,15 @@
+import {useState} from "react";
+
 const Form = (props) => {
   const{action, toggleViewHandler} = props
+
+  const [title, setTitle] = useState('')
+  const [amount, setAmount] = useState('')
+  const [date, setDate] = useState('')
+
+  const titleChangeHandler = (e) => setTitle(e.target.value)
+  const amountChangeHandler = (e) => setAmount(e.target.value)
+  const dateChangeHandler = (e) => setDate(e.target.value)
 
   return (
       <form
@@ -11,19 +21,21 @@ const Form = (props) => {
           className='add-expenses__input'
           type='text'
           placeholder='add a new expense'
+          onChange={titleChangeHandler}
         />
       </label>
 
       <label className='label'>
-        <span>Price</span>
+        <span>Amount</span>
         <input
           className='add-expenses__input'
           type='text'
           placeholder='amount of money spent'
+          onChange={amountChangeHandler}
         />
       </label>
 
-      <input type="date" className='add-expenses__input'/>
+      <input type="date" className='add-expenses__input' onChange={dateChangeHandler}/>
 
       <div className="add-expenses__container grid-row justify-content-start">
         <button
